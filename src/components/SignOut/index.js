aab6f608c1fe509f956cb573a8be41566d976160
PriyankaVaishya <priyankavaish129gmail.com>
2021-06-01 15:31:33 +0545

    Redirect to Landing page

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
- 
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
 import { withFirebase } from '../Firebase';
  
 const SignOutButton = ({ firebase }) => (
-  <button type="button" onClick={firebase.doSignOut}>
+  <button type="button" onClick={firebase.doSignOut}> <Link to={ROUTES.LANDING}>
     Sign Out
+    </Link>
   </button>
 );
  
