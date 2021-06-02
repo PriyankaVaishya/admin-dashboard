c42718fc876ac578a395650fbc51425ebd909e33
muskaanbakshi <72864112+muskaanbakshi@users.noreply.github.com>
2021-06-02 15:38:04 +0530

    Update index.js

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import { withFirebase } from '../Firebase';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import IconButton from '@material-ui/core/IconButton';
  
 const SignOutButton = ({ firebase }) => (
-  <button type="button" onClick={firebase.doSignOut}>
-    <Link to={ROUTES.LANDING}>
-    Sign Out
-    </Link>
-  </button>
+                <IconButton edge="start" color="inherit" aria-label="menu" onClick={firebase.doSignOut}>
+                        <Link to={ROUTES.LANDING}>
+                          <ExitToAppIcon />
+                          </Link>
+                </IconButton>
+
+
+  // <button type="button" onClick={firebase.doSignOut}>
+  //   <Link to={ROUTES.LANDING}>
+  //   Sign Out
+  //   </Link>
+  // </button>
 );
  
 export default withFirebase(SignOutButton);
