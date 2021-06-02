c705c97d69699569794053d548506a252479d6e7
muskaanbakshi <72864112+muskaanbakshi@users.noreply.github.com>
2021-06-02 12:35:32 +0530

    Landing and signout updated

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
  
 const LandingPage = () => (
-  <div>
-      LandingPage
+  <div style={{
+                backgroundImage: `url("https://i.pinimg.com/originals/4a/94/26/4a94268541d7a0ed95a8be5138e8a288.jpg")`,
+                backgroundRepeat: 'no-repeat',
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundSize: 'cover',
+                height:'832px',//'100vh',
+                margin: 0,
+                color:'white',
+                backgroundPosition: 'center',
+             }}>
+      <h1 style={{ fontSize: '3em' }}>Welcome to Admin-Dashboard</h1>
   </div>
 );
  
