6db9e515f9894a54f7095b46c16c1108071ce71f
PriyankaVaishya <priyankavaish129gmail.com>
2021-06-02 14:27:08 +0545

    Added side drawer and Accounts background

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -4,7 +4,18 @@ import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
  
 const AccountPage = () => (
-  <div>
+  <div style={{
+                        backgroundImage: `url("https://i.pinimg.com/originals/4a/94/26/4a94268541d7a0ed95a8be5138e8a288.jpg")`,
+                        backgroundRepeat: 'no-repeat',
+                        //display: "flex",
+                        //justifyContent: "center",
+                        //alignItems: "center",
+                        backgroundSize: 'cover',
+                        height:'480px',//'100vh',
+                        margin: 0,
+                        color:'white',
+                        backgroundPosition: 'center',
+                     }}>
     <h1>Account Page</h1>
     <PasswordForgetForm />
     <PasswordChangeForm />
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -8,41 +8,170 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import * as ROUTES from '../../constants/routes';
+import { withFirebase } from '../Firebase';
 
+import { withStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import HomeIcon from '@material-ui/icons/Home';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import clsx from 'clsx';
+import {  useTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+const drawerWidth = 240;
+
+//class Example extends React.Component {
+//  constructor(props) {
+//    super(props);
+//    this.state = {
+//      open: false
+//    };
+//  };
+//  };
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  appBar: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
+    align: 'center',
+  },
+  drawer: {
+  [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+      }
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
+   signOutButton: {
+        alignItems: 'center',
+    },
+
+  hide: {
+    display: 'none',
   },
 }));
- 
+// const handleDrawerOpen = () => {
+//     Example.state.open=true;
+//   };
+//
+//   const handleDrawerClose = () => {
+//    Example.state.open=false;
+//   };
 const Navigation = ({ authUser }) => (
   <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
 );
- 
-const NavigationAuth = () => (
+const NavigationAuth = (firebase) => (
   <div className={useStyles.root}>
-      <AppBar position="static">
+  <CssBaseline/>
+      <AppBar position="static"
+              className={clsx(useStyles.appBar, {
+                [useStyles.appBarShift]: false,
+              })}>
         <Toolbar>
-          <IconButton edge="start" className={useStyles.menuButton} color="inherit" aria-label="menu">
+          <IconButton edge="start"
+          className={clsx(useStyles.menuButton, true && useStyles.hide)}
+          color="inherit"
+          aria-label="open drawer"
+          //onClick={handleDrawerOpen}
+          >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={useStyles.title}>
             Hello Admin
           </Typography>
-          <Button color="inherit"><Link to={ROUTES.LANDING}>Landing</Link></Button>
-          <Button color="inherit"><Link to={ROUTES.HOME}>Home</Link></Button>
-          <Button color="inherit"><Link to={ROUTES.ACCOUNT}>Account</Link></Button>
-          <Button color="inherit"><SignOutButton /></Button>
         </Toolbar>
       </AppBar>
+<Drawer
+        className={useStyles.drawer}
+        variant="permanent"
+        anchor="left"
+       // Example.state.open={open}
+        classes={{
+          paper: useStyles.drawerPaper,
+        }}
+      >
+        <List>
+                      <ListItem>
+                        <ListItemIcon>
+                          <IconButton edge="start" color="inherit" aria-label="menu">
+                            <Link to={ROUTES.HOME}>
+                            <HomeIcon />
+                            </Link>
+                          </IconButton>
+                        </ListItemIcon>
+                        <ListItemText primary="Home" />
+                      </ListItem>
+                      <ListItem >
+                        <ListItemIcon>
+                        <IconButton edge="start" color="inherit" aria-label="menu">
+                            <Link to={ROUTES.LANDING}>
+                           <DashboardIcon />
+                           </Link>
+                          </IconButton>
+                        </ListItemIcon>
+                        <ListItemText primary="Landing Page" />
+                      </ListItem>
+                      <ListItem>
+                        <ListItemIcon>
+                        <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Link to={ROUTES.ACCOUNT}>
+                          <AccountCircleIcon />
+                          </Link>
+                          </IconButton>
+                        </ListItemIcon>
+                        <ListItemText primary="Account" />
+                      </ListItem>
+                      <ListItem >
+                      <SignOutButton />
+                      </ListItem>
+                    </List>
+      </Drawer>
     </div>
 
   // <ul>
@@ -71,10 +200,39 @@ const NavigationNonAuth = () => (
           <Typography variant="h6" className={useStyles.title}>
             Hello Admin
           </Typography>
-          <Button color="inherit"><Link to={ROUTES.LANDING}>Landing</Link></Button>
-          <Button color="inherit"> <Link to={ROUTES.SIGN_IN}>Sign In</Link></Button>
         </Toolbar>
       </AppBar>
+      <Drawer
+              className={useStyles.drawer}
+              variant="permanent"
+              anchor="left"
+              classes={{
+                paper: useStyles.drawerPaper,
+              }}
+            >
+              <List>
+                            <ListItem >
+                              <ListItemIcon>
+                              <IconButton edge="start" color="inherit" aria-label="menu">
+                                  <Link to={ROUTES.LANDING}>
+                                 <DashboardIcon />
+                                 </Link>
+                                </IconButton>
+                              </ListItemIcon>
+                              <ListItemText primary="Landing Page" />
+                            </ListItem>
+                            <ListItem>
+                              <ListItemIcon>
+                              <IconButton edge="start" color="inherit" aria-label="menu">
+                              <Link to={ROUTES.SIGN_IN}>
+                                <AccountCircleIcon />
+                                </Link>
+                                </IconButton>
+                              </ListItemIcon>
+                              <ListItemText primary="Sign In" />
+                            </ListItem>
+                          </List>
+            </Drawer>
     </div>
   // <ul>
   //   <li>
